4ed38e987ee7716b219f3d4afd20c4ce71552435
wangpeng <wangpeng@kezaihui.com>
2021-03-24 22:29:51 +0800

    341.NestedIterator

---
+++ src/main/java/leetcode/editor/cn/NestedInteger.java
@@ -0,0 +1,15 @@
+package leetcode.editor.cn;
+
+import java.util.List;
+
+/**
+ * @author wangpeng
+ * @date 2021-03-23
+ */
+interface NestedInteger {
+    public boolean isInteger();
+
+    public Integer getInteger();
+
+    public List<NestedInteger> getList();
+}
